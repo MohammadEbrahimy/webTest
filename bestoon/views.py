f23e9c9276d94a7dc4b17a440d87e95821a6f23a
MohammadEbrahimi <mohammaddxgi@gmail.com>
2020-02-08 10:58:22 +0330

    add url and view and solve first page not found error

--- bestoon/views.py
+++ bestoon/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def expense(request):
+    "this is for test"
+    print("This is my test process")
