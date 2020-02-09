e9ee72c6c4d8a04aa2d3fc1da872875528178e8a
MohammadEbrahimi <mohammaddxgi@gmail.com>
2020-02-09 21:15:57 +0330

    add home page template

--- web/views.py
+++ web/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("home")
+    #return HttpResponse("home")
+    return render(request, 'homepage.html')
